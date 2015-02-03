45c1508a56b3dc5ad698c23d145e22ab71142ff4
Andrew Lee <ajylee@gmail.com>
2015-02-03 14:00:40 -0800

    Fix newline in set1-challenge1

--- set1-challenge1.py
+++ set1-challenge1.py
@@ -2,13 +2,14 @@
 
 import toolz
 import binascii
+import base64
 
 test_input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
 test_output = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def b16_to_b64(ss):
-    return toolz.pipe(ss, binascii.a2b_hex, binascii.b2a_base64)
+    return toolz.pipe(ss, binascii.a2b_hex, base64.b64encode)
 
 
 assert b16_to_b64(test_input) == test_output
