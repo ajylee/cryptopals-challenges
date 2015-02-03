c709ca8763f61cdfe7ff5a2594509f216efc3994
Andrew Lee <ajylee@gmail.com>
2015-02-02 22:56:58 -0800

    Try chall 13

--- set2-challenge13.py
+++ set2-challenge13.py
@@ -1,11 +1,17 @@
 
-from toolz import valmap, identity
-from collections import OrderedDict
-import pyparsing as pp
-from pyparsing import (Word, alphas, alphanums, Forward, ZeroOrMore, Literal, Group,
-                       delimitedList, SkipTo)
-
+import re
 import string
+import random
+from collections import OrderedDict, Counter
+
+from toolz import valmap, identity, get
+import pyparsing as pp
+from pyparsing import (Word, alphas, alphanums, Forward, ZeroOrMore, Literal,
+                       Group, delimitedList, SkipTo)
+from Crypto.Cipher import AES
+
+from block_crypto import random_str, pad, chunks
+
 
 
 class KEV(object):
@@ -26,7 +32,10 @@ class KEV(object):
 
 class Profile(object):
     control_chars = '&='
-    invalid = [ii for ii in xrange(33)] + [ord(c) for c in control_chars]
+
+    # Screening nonprintable too strong?
+    # invalid = [ii for ii in xrange(33)] + [ord(c) for c in control_chars]
+    invalid = [ord(c) for c in control_chars]
     delete_table = {o:None for o in invalid}
     keys = ['email', 'uid', 'role']
 
@@ -45,45 +54,61 @@ class Profile(object):
         return KEV.encode(OrderedDict((k, dict_[k]) for k in cls.keys))
 
     @classmethod
-    def mk_valid(cls, tainted_data):
-        finalize = {'email': cls.remove_invalid,
-                    'uid': int,
-                    'role': cls.remove_invalid}
+    def finalize(cls, tainted_data):
+        _finalize = {'email': cls.remove_invalid,
+                     'uid': int,
+                     'role': cls.remove_invalid}
 
-        return {k: finalize[k](tainted_data) for k in cls.keys}
+        return {k: _finalize[k](tainted_data[k]) for k in cls.keys}
 
     @classmethod
     def decode(cls, strn):
-        return mk_valid(KEV.encode(strn))
+        return cls.finalize(KEV.decode(strn))
 
 
 class ProfileManager(object):
     def __init__(self):
         self.profiles = {}
-        self.uids = set()
+        self.uids = {0}
+        self.cipher = AES.new(random_str(16), AES.MODE_ECB)
+        self.pad_str = chr(4)
 
-    def unsafe_profile_for(self, email):
+    def unsafe_get_profile_for(self, email):
         _maybe_profile = self.profiles.get(email, None)
         if _maybe_profile:
-            return Profile.encode(_maybe_profile)
+            return _maybe_profile
         else:
             return None
 
-    def profile_for(self, email_strn):
-        valid_email = Profile.remove_invalid(email_strn)
-        return self.unsafe_profile_for(valid_email)
+    def profile_for(self, email):
+        valid_email = Profile.remove_invalid(email)
+        del email
+
+        maybe_profile = self.unsafe_get_profile_for(valid_email)
+
+        if maybe_profile:
+            profile = maybe_profile
+        else:
+            profile = {'email': valid_email,
+                       'uid': max(self.uids) + 1,
+                       'role': 'user'}
+
+            self.unsafe_add_profile(**profile)
+
+        return self.cipher.encrypt(pad(Profile.encode(profile),
+                                block_size=self.cipher.block_size,
+                                pad_str=self.pad_str))
+
+    def read_cookie(self, cookie):
+        decrypted = self.cipher.decrypt(cookie)
+        stripped = decrypted.rstrip(self.pad_str)
+        return Profile.decode(stripped)
 
     def unsafe_add_profile(self, email, uid, role):
+        self.uids.add(uid)
         self.profiles[email] = dict(email=email,
                                  uid=uid,
                                  role=role)
-
-    def add_profile(self, email, uid, role):
-        assert isinstance(uid, int)
-        self.unsafe_add_profile(
-            email = Profile.remove_invalid(email),
-            uid = uid,
-            role = Profile.remove_invalid(role))
 
 
 class Test(object):
@@ -111,7 +136,72 @@ class Test(object):
             assert Profile.encode(dec) == enc
 
 
-def test_attack_email():
+# Attack
+# -------
+
+DOMAIN = '@example.org'
+
+
+def random_username(length):
+    return ''.join(string.ascii_lowercase[random.randint(0,25)]
+               for ii in xrange(length))
+
+
+def mk_role_user_reverse_lookup(profile_for):
+    common_pads = [' ', chr(4), chr(8)]
+
+    usernames = [pad('user', block_size=16, pad_str=pad_str)
+               for pad_str in common_pads]
+
+    return {profile_for(username + DOMAIN)[:16]: username[-1]
+        for username in usernames}
+
+
+def try_repeatedly(thunk, max_tries):
+    tries = 0
+
+    while tries < max_tries:
+        maybe = thunk()
+        if maybe:
+            return maybe
+        else:
+            tries += 1
+
+    raise StandardError, 'not found'
+
+
+def get_base_cookie(profile_for, role_user_reverse_lookup):
+    """Get a username length such that the last block has only one content byte"""
+    for ii in xrange(1, 30):
+        cookie = profile_for(random_username(ii) + DOMAIN)
+        #print repr(cookie[-16:])
+        if cookie[-16:] in role_user_reverse_lookup:
+            return cookie, role_user_reverse_lookup[cookie[-16:]]
+
+
+def mk_admin_cookie(profile_for):
+    role_user_reverse_lookup = mk_role_user_reverse_lookup(profile_for)
+
+    thunk = lambda : get_base_cookie(profile_for, role_user_reverse_lookup)
+    base_cookie, pad_str = try_repeatedly(thunk, max_tries=20)
+    print repr(base_cookie)
+    admin_block = profile_for(pad('admin', block_size=block_size, pad_str=pad_str))[:16]
+    admin_cookie = base_cookie[:-16] + admin_block
+    return admin_cookie
+
+
+def test_mk_admin_cookie():
+    pman = ProfileManager()
+    cookie = mk_admin_cookie(pman.profile_for)
+    if cookie:
+        print pman.read_cookie(cookie)
+
+
+# Testing
+# --------
+
+def test_naive_attack_email():
+    # Test that naive attack fails
     legit_email = 'abc@example.com'
     attack_email = legit_email + '&uid=10&role=admin'
     valid_email = Profile.remove_invalid(attack_email)
@@ -121,12 +211,18 @@ def test_attack_email():
 
     pman = ProfileManager()
 
-    # add legit user
-    pman.add_profile(email=legit_email, uid=10, role='admin')
-    pman.unsafe_add_profile(email=attack_email, uid=11, role='user')
-    assert pman.unsafe_profile_for(attack_email).startswith(
-        pman.profile_for(legit_email))
-    assert pman.profile_for(attack_email) is None
+    # add legit admin user
+    pman.profile_for(email=legit_email)
+    pman.profiles[legit_email]['role'] = 'admin'
+
+    attack_cookie = pman.profile_for(email=attack_email)
+    attack_profile = pman.read_cookie(attack_cookie)
+    decrypted_cookie = pman.cipher.decrypt(attack_cookie)
+    assert attack_profile['role'] == 'user'
+    assert re.search('=user', decrypted_cookie)
+    assert not re.search('=admin', decrypted_cookie)
+
+
 
 
 if __name__ == '__main__':
@@ -134,6 +230,7 @@ if __name__ == '__main__':
     Test.test_profile_codec()
 
     #r = SkipTo(KEV.key, include=True).parseString('@abc')
-    test_attack_email()
+    test_naive_attack_email()
 
+    test_mk_admin_cookie()
     #profile_for('abc@example.com&uid=10&role=user')
