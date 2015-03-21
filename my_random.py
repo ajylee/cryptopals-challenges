c49f86c36fdb3865c45db2651fd0b31b5730b327
Andrew Lee <ajylee@gmail.com>
2015-03-20 22:41:02 -0700

    Fix some errors in my_random

--- my_random.py
+++ my_random.py
@@ -14,8 +14,8 @@ class MersenneTwister(object):
         self.state[0] = seed
 
         for ii in xrange(1, len(self.state)):
-            self.state[ii] =  _lowest_32_bits(
-                1812433253 * (self.state[ii - 1] ^ self.state[ii - 1] >> 30 ) + ii)
+            self.state[ii] = _lowest_32_bits(
+                1812433253 * (self.state[ii-1] ^ (self.state[ii-1] >> 30)) + ii)
 
     def extract_number(self):
         # Extract a tempered pseudorandom number based on the index-th value,
@@ -23,20 +23,21 @@ class MersenneTwister(object):
         if self.index == 0:
             self.generate_numbers()
 
-        self.index = (self.index + 1) % len(self.state)
+        nn = tz.pipe(self.state[self.index],
+                   lambda y: y ^ (y >> 11),
+                   lambda y: y ^ ((y << 7) & 2636928640),
+                   lambda y: y ^ ((y << 15) & 4022730752),
+                   lambda y: y ^ (y >> 18))
 
-        return tz.pipe(self.state[self.index],
-                     lambda y: y ^ (y >> 11),
-                     lambda y: y ^ ((y << 7) & 2636928640),
-                     lambda y: y ^ ((y << 15) & 4022730752),
-                     lambda y: y ^ (y >> 18))
+        self.index = (self.index + 1) % len(self.state)
+        return nn
 
     def generate_numbers(self):
         # Generate an array of 624 untempered numbers
 
         for ii in xrange(len(self.state)):
             y = ((self.state[ii] & 0x80000000) +
-                 (self.state[(ii+1) % len(self.state)] & 0x7fffffff))
+                 ((self.state[(ii+1) % len(self.state)]) & 0x7fffffff))
 
             self.state[ii] = self.state[(ii + 397) % len(self.state)] ^ (y >> 1)
             if y % 2 != 0:
@@ -45,11 +46,11 @@ class MersenneTwister(object):
 
 def test_mersenne_twister():
     seed = 0x3ab10e3471c1
-    mt = MersenneTwister(0)
+    mt = MersenneTwister(seed)
 
     count = {}
 
-    for ii in xrange(2 << 20 - 1):
+    for ii in xrange((2 << 20) - 1):
         if ii % (2 << 18) == 0:
             print len(bin(ii)) - 2, bin(ii)
 
@@ -57,13 +58,17 @@ def test_mersenne_twister():
         count[n] = count.get(n, 0) + 1
 
 
-    import matplotlib.pyplot as plt
-    import numpy as np
+    #import matplotlib.pyplot as plt
+    #import numpy as np
 
-    x = np.array(count.keys())
-    y = np.array([count[k] for k in x])
+    #x = np.array(count.keys())
+    #y = np.array([count[k] for k in x])
 
-    plt.ion()
-    plt.clf()
-    plt.plot(x, y, ls='None', marker='o')
-    plt.show()
+    #plt.ion()
+    #plt.clf()
+    #plt.plot(x, y, ls='None', marker='o')
+    #plt.show()
+
+
+if __name__ == '__main__':
+    test_mersenne_twister()
