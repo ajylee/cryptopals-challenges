43d1f19c4f055765bfb91e0d76a8d1f2e2234b71
Andrew Lee <ajylee@gmail.com>
2015-03-25 11:03:39 -0700

    Add set4-challeng25 (WIP)

---
+++ set4-challenge25.py
@@ -0,0 +1,42 @@
+from __future__ import division
+import Crypto.Random
+from block_crypto import CTR
+from bin_more import ords as str_to_ords
+
+BLOCK_SIZE = 16
+
+
+class EncryptedEditor(object):
+    def __init__(self):
+        rand_io = Crypto.Random.new()
+        key = rand_io.read(BLOCK_SIZE)
+        nonce = str_to_ords(rand_io.read(BLOCK_SIZE // 2))
+
+        self.cipher = CTR(key, nonce)
+
+    def encrypt(self, plaintext):
+        return self.cipher.encrypt(plaintext)
+
+    def decrypt(self, plaintext):
+        return self.encrypt(plaintext)
+
+    def edit(self, ciphertext, offset, new_text):
+        plaintext = self.cipher.decrypt(ciphertext)
+        new_plaintext = (plaintext[:offset]
+                         + new_text
+                         + plaintext[offset + len(new_text):])
+        return self.encrypt(new_plaintext)
+
+
+def test_edit():
+    ee = EncryptedEditor()
+    plaintext = 'this is the original plaintext'
+    ciphertext = ee.encrypt(plaintext)
+
+    offset = len('this is the ')
+    edited = ee.edit(ciphertext, offset, 'edited  ')
+    assert ee.decrypt(edited) == 'this is the edited   plaintext'
+
+
+if __name__ == '__main__':
+    test_edit()
