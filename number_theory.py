d83f023428bb1023935c200ab0f4159b45a6b2a0
Andrew Lee <ajylee@gmail.com>
2015-03-29 11:37:44 -0700

    Extract NIST_P_HEX

--- number_theory.py
+++ number_theory.py
@@ -3,6 +3,18 @@ import os
 
 DEBUG = False
 
+NIST_P_HEX = '''
+ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
+e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
+3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
+6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
+24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
+c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
+bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
+fffffffffffff'''.translate(bytearray(xrange(256)), ' \n')
+
+NIST_G = 2
+
 
 def byte_count(n):
     count = 0
@@ -57,16 +69,9 @@ def easy_diffie_hellman():
 
 
 def nist_diffie_hellman():
-    p = '''ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
-    e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
-    3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
-    6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
-    24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
-    c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
-    bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
-    fffffffffffff'''.translate(bytearray(xrange(256)), ' \n')
-
-    return diffie_hellman(p=long(p, 16), g=2)
+    p = NIST_P_HEX
+    g = NIST_G
+    return diffie_hellman(p=long(p, 16), g=g)
 
 
 if __name__ == '__main__':
