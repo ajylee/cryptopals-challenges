aa8c468771d06503e11945208b2f543be8b3ff4d
Andrew Lee <ajylee@gmail.com>
2015-03-31 20:29:27 -0700

    WIP

--- secure_remote_password.py
+++ secure_remote_password.py
@@ -12,6 +12,8 @@ import number_theory as nt
 LoginData = namedtuple('LoginData', [
     'N', 'g', 'k', 'email', 'password'])
 
+SessionData = namedtuple('SessionData', [
+    'salt', 'K'])
 
 def _int_to_str(nn):
     hex_rep = hex(nn).lstrip('0x').rstrip('L')
@@ -35,72 +37,67 @@ def calculate_x(salt, password):
     xH = sha256(_int_to_str(salt) + password).digest()
     return _str_to_int(xH)
 
+    
+def calculate_K_client(B, k, g, x, a, u, N):
+    # S = (B - k * g**x)**(a + u * x) % N
+    S = nt.modexp(B - k * nt.modexp(g, x, N), (a + u * x), N)
+    K = sha256(_int_to_str(S)).digest()
+    return K
+
+
+def calculate_K_server(A, v, u, b, N):
+    #  S = (A * v**u) ** b % N
+    S = nt.modexp(A * nt.modexp(v, u, N), b, N)
+    K = sha256(_int_to_str(S)).digest()
+    return K
+
+
+def gen_bB_server(login_data, v):
+    dat = login_data
+    b = dh.mod_random(dat.N)
+    B = dat.k * v + nt.modexp(dat.g, b, dat.N) 
+    return b, B
+
+
+def gen_salt_and_v_server(login_data):
+    dat = login_data
+    salt = gen_salt()
+    x = calculate_x(salt, dat.password)
+    v = nt.modexp(dat.g, x, dat.N)
+    return salt, v
+
 
 class Server(object):
     def __init__(self):
         self.login_data = {}
-        self.handshake_data = {}
+        self.session_data = {}
         self._session_count = 0
 
     def new_session(self):
         _id = self._session_count 
         self._session_count += 1
-        self.handshake_data[_id] = {}
+        self.session_data[_id] = {}
         return _id
 
-    def gen_salt_and_v(self, session_id):
-        client_id = self.handshake_data[session_id]['client_id']
-        dat = self.login_data[client_id]
 
-        salt = gen_salt()
-
-        x = calculate_x(salt, dat.password)
-
-        v = nt.modexp(dat.g, x, dat.N)
-
-        self.handshake_data[session_id]['salt'] = salt
-        self.handshake_data[session_id]['v'] = v
-
-    def gen_bB(self, session_id):
-        client_id = self.handshake_data[session_id]['client_id']
-        dat = self.login_data[client_id]
-
-        b = dh.mod_random(dat.N)
-        v = self.handshake_data[session_id]['v']
-        B = dat.k * v + nt.modexp(dat.g, b, dat.N) 
-        return b, B
-
-    def get_K(self, session_id, A, b, B):
-        _handshake_data = self.handshake_data[session_id]
-        client_id = _handshake_data['client_id']
-        dat = self.login_data[client_id]
-
-        u = calculate_u(A, B)
-        
-        #  S = (A * v**u) ** b % N
-        S = nt.modexp(A * nt.modexp(_handshake_data['v'], u, dat.N), b, dat.N)
-        K = sha256(_int_to_str(S)).digest()
-        
-        _handshake_data['K'] = K
-        return K
-                         
     def respond_handshake(self):
         session_id = self.new_session()
-        _handshake_data = self.handshake_data[session_id]
 
         client_id = yield
 
-        _handshake_data['client_id'] = client_id
-        self.gen_salt_and_v(session_id)
-        salt = _handshake_data['salt']
+        dat = self.login_data[client_id]
+        salt, v = gen_salt_and_v_server(dat)
 
         A = yield 
 
-        b, B = self.gen_bB(session_id)
+        b, B = gen_bB_server(dat, v)
+
+        u = calculate_u(A, B)
+        K = calculate_K_server(A, v, u, b, dat.N)
+
+        self.session_data[session_id] = SessionData(salt, K)
+
         client_hmac = yield (salt, B)
-
-        K = self.get_K(session_id, A, b, B)
-
         my_hmac = HMAC(K, _int_to_str(salt)).hexdigest()
 
         if my_hmac == client_hmac:
@@ -115,11 +112,12 @@ class Client(object):
                                'normal client')
 
         self.my_login_data = None
-        self.handshake_data = {}
+        self.handshake_data = None
 
     def conduct_handshake(self, server):
         response_delegate = server.respond_handshake()
         response_delegate.next()
+
         response_delegate.send(self.user_id)
 
         dat = self.my_login_data
@@ -131,28 +129,33 @@ class Client(object):
 
         u = calculate_u(A, B)
         x = calculate_x(salt, self.my_login_data.password)
+        K = calculate_K_client(B, dat.k, dat.g, x, a, u, dat.N)
 
-        # S = (B - k * g**x)**(a + u * x) % N
-        S = nt.modexp(B - dat.k * nt.modexp(dat.g, x, dat.N), (a + u * x), dat.N)
-        K = sha256(_int_to_str(S)).digest()
-
-
-        self.handshake_data['K'] = K
-        self.handshake_data['salt'] = salt
+        self.handshake_data = SessionData(salt, K)
 
         hmac = HMAC(K, _int_to_str(salt)).hexdigest()
         validation_message = response_delegate.send(hmac)
         assert validation_message == 'OK'
 
 
-def signup(server, client, email, password):
-    data = LoginData(
+def mk_login_data(email, password):
+    return LoginData(
         N = long(dh.NIST_P_HEX, 16),
         g = 2,
         k = 3,
         email = email,
         password = password,
     )
+
+
+CLIENT_LOGIN_DATA = mk_login_data(
+    email = 'abc@example.org',
+    password = 'some_password',
+)
+
+
+
+def signup(server, client, email, password):
 
     server.login_data[client.user_id] = data 
     client.my_login_data = data
@@ -161,9 +164,6 @@ def signup(server, client, email, password):
 def test_SRP():
     s = Server()
     c = Client()
-
-    email = 'abc@example.org'
-    password = 'some_password'
 
     signup(s, c, email, password)
 
