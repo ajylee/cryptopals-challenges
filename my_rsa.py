ba904ddadcd58eabdc64b0c2081f1eeb23f8f317
Andrew Lee <ajylee@gmail.com>
2015-04-03 13:06:07 -0700

    Generalize my_rsa to any length text

--- my_rsa.py
+++ my_rsa.py
@@ -1,12 +1,23 @@
 
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util.number import getPrime, long_to_bytes, bytes_to_long
 import Crypto.Random
 import number_theory as nt
+import toolz as tz
+import toolz.curried as tzc
+
+
+BLOCK_SIZE = 16
+
+@tz.curry
+def partition_str(size, strn):
+    for ii in xrange(0, len(strn), size):
+        yield strn[ii:ii + size]
 
 
 def keygen():
-    # => p * q has > 16 bytes => Can encrypt 16 byte blocks
-    prime_size_bits = 8 * 8 + 1  
+    # => p * q has 16 bytes => Can encrypt 15 byte blocks as 16 byte blocks
+    prime_size_bits = 8 * 8 + 1
 
     #p, q = getPrime(prime_size), getPrime(prime_size)
 
@@ -38,19 +49,47 @@ def str_modexp(strn, e, n):
 def encrypt(public_key, message):
     # c = m**e%n
     e, n = public_key
-    return str_modexp(message, e, n)
+
+    def _encrypt_block(block):
+        prepadded = chr(1) + block
+        c = str_modexp(prepadded, e, n)
+        return chr(0) * (2 * BLOCK_SIZE - len(c)) + c
+
+    return tz.pipe(
+        message,
+        partition_str(BLOCK_SIZE - 1),
+        tzc.map(_encrypt_block),
+        ''.join)
 
 
 def decrypt(private_key, ciphertext):
     # m = c**d%n
     d, n = private_key
-    return str_modexp(ciphertext, d, n)
+
+    def _decrypt_block(cblock):
+        prepadded = str_modexp(cblock, d, n)
+        block = prepadded[1:] # remove \x01 pad
+        return block
+
+    return tz.pipe(
+        ciphertext,
+        partition_str(2 * BLOCK_SIZE),
+        tzc.map(_decrypt_block),
+        ''.join)
+
+
+def encrypt_nopad(public_key, message):
+    return str_modexp(message, *public_key)
+
+
+def decrypt_nopad(private_key, ciphertext):
+    return str_modexp(ciphertext, *private_key)
 
 
 def test_rsa():
     pubkey, privkey = keygen()
 
-    message = Crypto.Random.new().read(16)
+    message = Crypto.Random.new().read(100)
 
     c  = encrypt(pubkey, message)
     m1 = decrypt(privkey, c)
@@ -58,5 +97,21 @@ def test_rsa():
     assert m1 == message
 
 
+def test_rsa_nopad():
+    pubkey, privkey = keygen()
+
+    # message must not begin with \x00
+    message = chr(1) + Crypto.Random.new().read(15)
+
+    c  = encrypt_nopad(pubkey, message)
+    m1 = decrypt_nopad(privkey, c)
+
+    assert m1 == message
+
+
 if __name__ == '__main__':
-    test_rsa()
+    for ii in xrange(10):
+        test_rsa()
+
+    for ii in xrange(10):
+        test_rsa_nopad()
