eaf24f255c6fa7b1eb7518c46069f1a54c576def
Andrew Lee <ajylee@gmail.com>
2015-04-05 23:20:15 -0700

    Implement chall48 (inside chall47 file)

--- set6-challenge47.py
+++ set6-challenge47.py
@@ -3,29 +3,36 @@ import logging
 import pkcs1
 from my_rsa import keygen, encrypt, BLOCK_SIZE
 from bleichenbacher import pkcs1_oracle, search
-
-BLOCK_SIZE = 256 // 8
-
 from memo import memo
 
 
-def test_recover_plaintext():
+def recover_plaintext_test_tool(block_size):
     plaintext =  "kick it, CC"
 
-    pubkey, privkey = memo(__name__ + '.keygen', lambda : keygen(BLOCK_SIZE))
+    pubkey, privkey = memo(__name__ + '.keygen', lambda : keygen(block_size))
 
-    ciphertext = encrypt(pubkey, pkcs1.pad(plaintext, chr(2), BLOCK_SIZE))
+    ciphertext = encrypt(pubkey, pkcs1.pad(plaintext, chr(2), block_size))
 
-    _oracle = pkcs1_oracle(privkey, BLOCK_SIZE)
+    _oracle = pkcs1_oracle(privkey, block_size)
 
     assert _oracle(ciphertext)
 
     ok_padding, solution = pkcs1.check_and_remove_padding(
-        search(_oracle, BLOCK_SIZE, pubkey, ciphertext),
+        search(_oracle, block_size, pubkey, ciphertext),
         chr(2))
 
     
     assert solution == plaintext, solution
+
+    logging.info('solution matches plaintext')
+
+
+def test_recover_plaintext_47():
+    recover_plaintext_test_tool(256 // 8)
+
+
+def test_recover_plaintext_48():
+    recover_plaintext_test_tool(768 // 8)
 
 
 if __name__ == '__main__':
@@ -34,4 +41,5 @@ if __name__ == '__main__':
     bleichenbacher.logger.setLevel(logging.DEBUG)
     logging.getLogger().setLevel(logging.DEBUG)
 
-    test_recover_plaintext()
+    test_recover_plaintext_47()
+    test_recover_plaintext_48()
